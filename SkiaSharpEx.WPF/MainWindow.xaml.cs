f191e7290c6b3dc489b98134714daae2f2148fa7
S Ravi Kumar <xltants@xltants.com>
2018-06-27 15:33:13 +0530

    Updated Nuget Packages and added WPF Project

---
+++ SkiaSharpEx.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace SkiaSharpEx.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new SkiaSharpEx.App());
+        }
+    }
+}
